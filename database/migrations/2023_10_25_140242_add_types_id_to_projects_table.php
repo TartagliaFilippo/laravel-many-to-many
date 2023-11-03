7f0d223f34bec6419a1bbf120fa9a296f452397e
filippo199600 <pippotartaglia@gmail.com>
2023-11-03 14:52:36 +0100

    fix refresh db

--- database/migrations/2023_10_25_140242_add_types_id_to_projects_table.php
+++ database/migrations/2023_10_25_140242_add_types_id_to_projects_table.php
@@ -29,8 +29,8 @@ return new class extends Migration {
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->dropForeign('projects_types_id_foreign');
-            $table->dropColumn('types_id');
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
